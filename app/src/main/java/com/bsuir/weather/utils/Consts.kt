c51b4b25c099268a8656db0fe9ba4eabe2630f7b
Egor Pankratiev <egor.pankratiew@gmail.com>
2025-04-19 13:39:35 +0300

    updated default location model and add address fetching logic

--- app/src/main/java/com/bsuir/weather/utils/Consts.kt
+++ app/src/main/java/com/bsuir/weather/utils/Consts.kt
@@ -1,11 +1,20 @@
 package com.bsuir.weather.utils
 
+import com.bsuir.weather.domain.model.AddressModel
 import com.bsuir.weather.domain.model.LocationModel
 
 const val mapZoom = 15f
 
 val defaultLocation = LocationModel(
-    latitude = 53.911,
-    longitude = 27.593,
-    //name = "Минск"
+    latitude = 53.9115384,
+    longitude = 27.5935445,
+    address = AddressModel(
+        countryName = "Беларусь",
+        locality = "Минск",
+        subLocality = "Советский район",
+        adminArea = "Минская область",
+        subAdminArea = "Минский район",
+        thoroughfare = "улица Гикало",
+        subThoroughfare = "24/1",
+    )
 )
