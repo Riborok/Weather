46bb0239d69a56717cf771a70057e1d66ef89fe9
Stanislaw Habrus <stas.gabrus@gmail.com>
2025-04-19 13:12:41 +0300

    added HiltViewModel annotation to PickedLocationViewModel

--- app/src/main/java/com/bsuir/weather/presentation/viewmodel/PickedLocationViewModel.kt
+++ app/src/main/java/com/bsuir/weather/presentation/viewmodel/PickedLocationViewModel.kt
@@ -2,10 +2,13 @@ package com.bsuir.weather.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.bsuir.weather.domain.model.LocationModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
 
-class PickedLocationViewModel: ViewModel() {
+@HiltViewModel
+class PickedLocationViewModel @Inject constructor(): ViewModel() {
     private val _pickedLocation = MutableStateFlow<LocationModel?>(null)
     val pickedLocation: StateFlow<LocationModel?> = _pickedLocation
 
