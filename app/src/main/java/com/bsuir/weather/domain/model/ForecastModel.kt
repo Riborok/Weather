b744d09374c248a225aed539f45f8284fe654aa9
Stanislaw Habrus <stas.gabrus@gmail.com>
2025-05-04 15:09:30 +0300

    extended daily forecast model

--- app/src/main/java/com/bsuir/weather/domain/model/ForecastModel.kt
+++ app/src/main/java/com/bsuir/weather/domain/model/ForecastModel.kt
@@ -9,66 +9,120 @@ data class ForecastModel (
     val dailyForecasts: List<DailyForecastModel>
 )
 
-data class CurrentForecastModel (
+data class CurrentForecastModel(
+    val time: LocalDateTime,
+
     val temperature: Int,
     val apparentTemperature: Int,
+
     val relativeHumidity: Int,
     val precipitation: Int,
     val cloudCover: Int,
-    val iconId: Int,
-    val weatherDescriptionId: Int,
+
     val surfacePressure: Int,
     val pressureMSL: Int,
+
     val windSpeed: Int,
-    val windDirectionId: Int,
     val windGusts: Int,
-    val time: LocalDateTime,
+    val windDirectionId: Int,
+
+    val iconId: Int,
+    val weatherDescriptionId: Int
 )
 
 data class HourlyForecastModel(
+    val time: LocalDateTime,
+
     val temperature: Int,
-    val iconId: Int,
-    val weatherDescriptionId: Int,
-    val relativeHumidity: Int,
     val apparentTemperature: Int,
-    val precipitationProbability: Int,
-    val surfacePressure: Int,
-    val cloudCover: Int,
-    val visibility: Int,
-    val windSpeed10m: Int,
-    val windDirectionId10m: Int,
-    val windGusts10m: Int,
-    val uvIndex: Double,
-    val uvIndexClearSky: Double,
-    val pressureMsl: Int,
-    val precipitation: Int,
-    val windSpeed80m: Int,
-    val windSpeed120m: Int,
-    val windSpeed180m: Int,
-    val windDirectionId80m: Int,
-    val windDirectionId120m: Int,
-    val windDirectionId180m: Int,
     val temperature80m: Int,
     val temperature120m: Int,
     val temperature180m: Int,
+
+    val relativeHumidity: Int,
+    val precipitation: Int,
+    val precipitationProbability: Int,
+    val cloudCover: Int,
+
+    val surfacePressure: Int,
+    val pressureMsl: Int,
+
+    val uvIndex: Double,
+    val uvIndexClearSky: Double,
+
+    val visibility: Int,
+
+    val windSpeed10m: Int,
+    val windSpeed80m: Int,
+    val windSpeed120m: Int,
+    val windSpeed180m: Int,
+
+    val windGusts10m: Int,
+
+    val windDirectionId10m: Int,
+    val windDirectionId80m: Int,
+    val windDirectionId120m: Int,
+    val windDirectionId180m: Int,
+
     val soilTemperature0cm: Int,
     val soilTemperature6cm: Int,
     val soilTemperature18cm: Int,
     val soilTemperature54cm: Int,
+
     val soilMoisture0to1cm: Double,
     val soilMoisture1to3cm: Double,
     val soilMoisture3to9cm: Double,
     val soilMoisture9to27cm: Double,
     val soilMoisture27to81cm: Double,
-    val time: LocalDateTime,
+
+    val iconId: Int,
+    val weatherDescriptionId: Int
 )
 
-data class DailyForecastModel (
+data class DailyForecastModel(
+    val date: LocalDate,
     val iconId: Int,
     val weatherDescriptionId: Int,
+
     val minTemperature: Int,
     val maxTemperature: Int,
+    val meanTemperature: Int,
+
     val sunrise: LocalDateTime,
     val sunset: LocalDateTime,
-    val date: LocalDate,
+    val daylightDuration: Int,
+
+    val uvIndexMax: Double,
+    val uvIndexClearSkyMax: Double,
+
+    val windSpeed10mMin: Int,
+    val windSpeed10mMean: Int,
+    val windSpeed10mMax: Int,
+
+    val windGusts10mMin: Int,
+    val windGusts10mMean: Int,
+    val windGusts10mMax: Int,
+
+    val windDirectionId10mDominant: Int,
+
+    val precipitationSum: Int,
+    val precipitationProbabilityMean: Int,
+
+    val relativeHumidityMean: Int,
+
+    val surfacePressureMin: Int,
+    val surfacePressureMean: Int,
+    val surfacePressureMax: Int,
+
+    val pressureMslMin: Int,
+    val pressureMslMean: Int,
+    val pressureMslMax: Int,
+
+    val visibilityMin: Int,
+    val visibilityMean: Int,
+    val visibilityMax: Int,
+
+    val cloudCoverMin: Int,
+    val cloudCoverMean: Int,
+    val cloudCoverMax: Int
 )
--- app/src/main/java/com/bsuir/weather/utils/mapper/WeatherResponseMapper.kt
+++ app/src/main/java/com/bsuir/weather/utils/mapper/WeatherResponseMapper.kt
@@ -23,64 +23,76 @@ object WeatherResponseMapper {
 
     private fun Current.toModel(): CurrentForecastModel {
         return CurrentForecastModel(
+            time = time,
+
             temperature = temperature2m.roundToInt(),
             apparentTemperature = apparentTemperature.roundToInt(),
+
             relativeHumidity = relativeHumidity2m,
             precipitation = precipitation.roundToInt(),
             cloudCover = cloudCover,
-            iconId = WeatherCodeConverter.getWeatherIconId(weatherCode, isDay == 1),
-            weatherDescriptionId = WeatherCodeConverter.getWeatherDescriptionId(weatherCode),
+
             surfacePressure = surfacePressure.roundToInt(),
             pressureMSL = pressureMsl.roundToInt(),
+
             windSpeed = windSpeed10m.roundToInt(),
-            windDirectionId = WeatherWindDirectionConverter.getWindDirectionId(windDirection10m),
             windGusts = windGusts10m.roundToInt(),
-            time = time
+            windDirectionId = WeatherWindDirectionConverter.getWindDirectionId(windDirection10m),
+
+            iconId = WeatherCodeConverter.getWeatherIconId(weatherCode, isDay == 1),
+            weatherDescriptionId = WeatherCodeConverter.getWeatherDescriptionId(weatherCode)
         )
     }
 
     private fun Hourly.toModels(): List<HourlyForecastModel> {
         return time.mapIndexed { index, time ->
             HourlyForecastModel(
+                time = time,
+
                 temperature = temperature2m[index].roundToInt(),
-                iconId = WeatherCodeConverter.getWeatherIconId(weatherCode[index], isDay[index] == 1),
-                weatherDescriptionId = WeatherCodeConverter.getWeatherDescriptionId(weatherCode[index]),
-                relativeHumidity = relativeHumidity2m[index],
                 apparentTemperature = apparentTemperature[index].roundToInt(),
-                precipitationProbability = precipitationProbability[index],
-                surfacePressure = surfacePressure[index].roundToInt(),
-                cloudCover = cloudCover[index],
-                visibility = visibility[index].roundToInt(),
-                windSpeed10m = windSpeed10m[index].roundToInt(),
-                windDirectionId10m =
-                    WeatherWindDirectionConverter.getWindDirectionId(windDirection10m[index]),
-                windGusts10m = windGusts10m[index].roundToInt(),
-                uvIndex = uvIndex[index],
-                uvIndexClearSky = uvIndexClearSky[index],
-                pressureMsl = pressureMsl[index].roundToInt(),
-                precipitation = precipitation[index].roundToInt(),
-                windSpeed80m = windSpeed80m[index].roundToInt(),
-                windSpeed120m = windSpeed120m[index].roundToInt(),
-                windSpeed180m = windSpeed180m[index].roundToInt(),
-                windDirectionId80m =
-                    WeatherWindDirectionConverter.getWindDirectionId(windDirection80m[index]),
-                windDirectionId120m =
-                    WeatherWindDirectionConverter.getWindDirectionId(windDirection120m[index]),
-                windDirectionId180m =
-                    WeatherWindDirectionConverter.getWindDirectionId(windDirection180m[index]),
                 temperature80m = temperature80m[index].roundToInt(),
                 temperature120m = temperature120m[index].roundToInt(),
                 temperature180m = temperature180m[index].roundToInt(),
+
+                relativeHumidity = relativeHumidity2m[index],
+                precipitation = precipitation[index].roundToInt(),
+                precipitationProbability = precipitationProbability[index],
+                cloudCover = cloudCover[index],
+
+                surfacePressure = surfacePressure[index].roundToInt(),
+                pressureMsl = pressureMsl[index].roundToInt(),
+
+                uvIndex = uvIndex[index],
+                uvIndexClearSky = uvIndexClearSky[index],
+
+                visibility = visibility[index].roundToInt(),
+
+                windSpeed10m = windSpeed10m[index].roundToInt(),
+                windSpeed80m = windSpeed80m[index].roundToInt(),
+                windSpeed120m = windSpeed120m[index].roundToInt(),
+                windSpeed180m = windSpeed180m[index].roundToInt(),
+
+                windGusts10m = windGusts10m[index].roundToInt(),
+
+                windDirectionId10m = WeatherWindDirectionConverter.getWindDirectionId(windDirection10m[index]),
+                windDirectionId80m = WeatherWindDirectionConverter.getWindDirectionId(windDirection80m[index]),
+                windDirectionId120m = WeatherWindDirectionConverter.getWindDirectionId(windDirection120m[index]),
+                windDirectionId180m = WeatherWindDirectionConverter.getWindDirectionId(windDirection180m[index]),
+
                 soilTemperature0cm = soilTemperature0cm[index].roundToInt(),
                 soilTemperature6cm = soilTemperature6cm[index].roundToInt(),
                 soilTemperature18cm = soilTemperature18cm[index].roundToInt(),
                 soilTemperature54cm = soilTemperature54cm[index].roundToInt(),
+
                 soilMoisture0to1cm = soilMoisture0to1cm[index],
                 soilMoisture1to3cm = soilMoisture1to3cm[index],
                 soilMoisture3to9cm = soilMoisture3to9cm[index],
                 soilMoisture9to27cm = soilMoisture9to27cm[index],
                 soilMoisture27to81cm = soilMoisture27to81cm[index],
-                time = time
+
+                iconId = WeatherCodeConverter.getWeatherIconId(weatherCode[index], isDay[index] == 1),
+                weatherDescriptionId = WeatherCodeConverter.getWeatherDescriptionId(weatherCode[index])
             )
         }
     }
@@ -88,13 +100,54 @@ object WeatherResponseMapper {
     private fun Daily.toModels(): List<DailyForecastModel> {
         return date.mapIndexed { index, date ->
             DailyForecastModel(
-                iconId = WeatherCodeConverter.getWeatherIconId(weatherCode[index], true),
-                weatherDescriptionId = WeatherCodeConverter.getWeatherDescriptionId(weatherCode[index]),
+                date = date,
+
                 minTemperature = temperature2mMin[index].roundToInt(),
                 maxTemperature = temperature2mMax[index].roundToInt(),
+                meanTemperature = temperature2mMean[index].roundToInt(),
+
                 sunrise = sunrise[index],
                 sunset = sunset[index],
-                date = date
+                daylightDuration = daylightDuration[index].roundToInt(),
+
+                precipitationSum = precipitationSum[index].roundToInt(),
+                precipitationProbabilityMean = precipitationProbabilityMean[index],
+
+                cloudCoverMean = cloudCoverMean[index],
+                cloudCoverMin = cloudCoverMin[index],
+                cloudCoverMax = cloudCoverMax[index],
+
+                relativeHumidityMean = relativeHumidity2mMean[index],
+
+                surfacePressureMean = surfacePressureMean[index].roundToInt(),
+                surfacePressureMin = surfacePressureMin[index].roundToInt(),
+                surfacePressureMax = surfacePressureMax[index].roundToInt(),
+
+                pressureMslMean = pressureMslMean[index].roundToInt(),
+                pressureMslMin = pressureMslMin[index].roundToInt(),
+                pressureMslMax = pressureMslMax[index].roundToInt(),
+
+                visibilityMean = visibilityMean[index].roundToInt(),
+                visibilityMin = visibilityMin[index].roundToInt(),
+                visibilityMax = visibilityMax[index].roundToInt(),
+
+                uvIndexMax = uvIndexMax[index],
+                uvIndexClearSkyMax = uvIndexClearSkyMax[index],
+
+                windSpeed10mMin = windSpeed10mMin[index].roundToInt(),
+                windSpeed10mMean = windSpeed10mMean[index].roundToInt(),
+                windSpeed10mMax = windSpeed10mMax[index].roundToInt(),
+
+                windGusts10mMin = windGusts10mMin[index].roundToInt(),
+                windGusts10mMean = windGusts10mMean[index].roundToInt(),
+                windGusts10mMax = windGusts10mMax[index].roundToInt(),
+
+                windDirectionId10mDominant = WeatherWindDirectionConverter.getWindDirectionId(
+                    windDirection10mDominant[index]
+                ),
+
+                iconId = WeatherCodeConverter.getWeatherIconId(weatherCode[index], true),
+                weatherDescriptionId = WeatherCodeConverter.getWeatherDescriptionId(weatherCode[index])
             )
         }
     }
