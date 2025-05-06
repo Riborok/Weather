1e3e136d5cc25ace37b95b7c3bdb153202c296fe
Egor Pankratiev <egor.pankratiew@gmail.com>
2025-05-06 17:43:16 +0300

    added WeatherNotifier and WeatherNotifierStringFormatter for weather notifications; created XML layouts for notification display

---
+++ app/src/main/java/com/bsuir/weather/notification/WeatherNotifierStringFormatter.kt
@@ -0,0 +1,44 @@
+package com.bsuir.weather.notification
+
+import android.content.Context
+import com.bsuir.weather.R
+import com.bsuir.weather.domain.model.AddressModel
+import com.bsuir.weather.utils.ext.formattedOrUnknown
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.toJavaLocalDateTime
+import java.time.format.DateTimeFormatter
+
+class WeatherNotifierStringFormatter(private val context: Context) {
+
+    fun formatLocation(address: AddressModel): String {
+        return address.formattedOrUnknown(context)
+    }
+
+    fun formatTemperature(temp: Int): String {
+        return str(R.string.degree, temp)
+    }
+
+    fun formatFeelsLike(feelsLikeTemp: Int): String {
+        return str(R.string.feels_like, formatTemperature(feelsLikeTemp))
+    }
+
+    fun formatTitle(location: String, temperature: Int): String {
+        return "$location: ${formatTemperature(temperature)}"
+    }
+
+    fun formatContentText(feelsLikeTemp: Int, descriptionId: Int): String {
+        return "${formatFeelsLike(feelsLikeTemp)}, ${str(descriptionId)}"
+    }
+
+    fun formatHour(dateTime: LocalDateTime): String {
+        return dateTime.toJavaLocalDateTime().format(DateTimeFormatter.ofPattern("H:mm"))
+    }
+
+    fun formatDescription(descriptionId: Int): String {
+        return context.getString(descriptionId)
+    }
+
+    private fun str(resId: Int, vararg args: Any): String {
+        return context.getString(resId, *args)
+    }
+}
